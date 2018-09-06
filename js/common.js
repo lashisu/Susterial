900ade100da1a514176fe21f475b6757ae75d0fb
Sushil Kumar <sushil.gu@chilindo.com>
2018-09-06 11:29:26 +0700

    js changes

--- js/common.js
+++ js/common.js
@@ -25,7 +25,6 @@ $(document).ready(function () {
         $('.swal2-warning').show().removeClass('pulse-warning');
     });
 
-
     $(document).on('click', '.navbar-toggler', function () {
         if ($(this).hasClass('toggled')) {
             $('html').removeClass('nav-open');
@@ -65,16 +64,7 @@ $(document).ready(function () {
         }
     });
 
-    $(document).on("focus", ".form-control", function () {
-        $(this).parents(".form-group").addClass("is-focused");
-    }).on("blur", ".form-control", function () {
-        $(this).parents(".form-group").removeClass("is-focused");
-        if ($(this).val() != "") {
-            $(this).parents(".form-group").addClass("is-filled");
-        } else {
-            $(this).parents(".form-group").removeClass("is-filled");
-        }
-    });
+
 
     $(document).on('click', '[data-toggle-src]', function (e) {
         e.preventDefault();
